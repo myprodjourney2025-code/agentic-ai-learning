823a580296f191bbd5c8a73ca0d16ca0135f41b8
Samiran Setua <samiransetua26@gmail.com>
2026-08-19 14:23:56 +0530

    feat: implement latest information retrieval tool and integrate with agent

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { config } from "dotenv";
 import { ChatMistralAI } from "@langchain/mistralai";
 import { input } from "@inquirer/prompts";
-import { HumanMessage, AIMessage, SystemMessage } from "langchain";
+import {
+  HumanMessage,
+  AIMessage,
+  SystemMessage,
+  tool,
+  createAgent,
+} from "langchain";
+import * as z from "zod";
 
 config();
 
@@ -39,6 +46,32 @@ const model = new ChatMistralAI({
 //   process.stdout.write("\n");
 // }
 
+function getLatestInformation({ query }: { query: string }): string {
+  return `Latest information about "${query} is:
+  1. India plans a phased ban on imports of 405 defence items to strengthen domestic manufacturing.
+  2. Omar Abdullah meets a US envoy in Jammu and Kashmir, saying the envoy came to listen to local concerns.
+  3. Husband and mother-in-law charged over the death of an Indian bride.
+  4. Women-led protests in India continue to demand political and social change.
+  5. Student and school-related protests against the Modi government gain attention.
+   "`;
+}
+
+const getLatestInfoTool = tool(getLatestInformation, {
+  name: "getLatestInformation",
+  description: "Get the latest information about a topic or query.",
+  schema: z.object({
+    query: z
+      .string()
+      .describe("The topic or query to get the latest information about."),
+  }),
+});
+
+const agent = createAgent({
+  name: "InfoBot",
+  model,
+  tools: [getLatestInfoTool],
+});
+
 const messageHistory: any = [
   new SystemMessage(
     "Hey, Your name is VERONICA. You are a helpful assistant. You are a large language model trained by Mistral AI. You will answer my questions and help me in my day to day life works. You will answer in a friendly and helpful manner.",
@@ -50,11 +83,17 @@ while (true) {
 
   messageHistory.push(new HumanMessage(userPrompt));
 
-  const stream = await model.stream(messageHistory);
+  //   const stream = await model.stream(messageHistory);
+  const stream = await agent.stream(
+    { messages: messageHistory },
+    {
+      streamMode: "messages",
+    },
+  );
 
   let aiResponse = "";
 
-  for await (const chunk of stream) {
+  for await (const [chunk] of stream) {
     process.stdout.write(chunk.text);
     aiResponse += chunk.text;
   }
